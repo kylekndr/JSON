2bd45a87e58f3244ca4d404ac2e6958548cd0801
kylekndr <36177416+kylekndr@users.noreply.github.com>
2018-04-09 18:26:51 -0500

    JavaScript For Movie Poster Inventory
    JS code for creating a multiple property inventory list using a flexible array.

---
+++ PosterInventory.js
@@ -0,0 +1,53 @@
+//JavaScript used to create a flexible array with multiple properties. Great for storing inventory information:
+var myPosters = [
+  {
+    "Title": "Star Wars",
+    "Version": "Style A",
+    "Film-Year": 1977,
+    "Size": [ 
+      "One-Sheet", 
+      "Lobby-Card", 
+      "Daybill" ],
+    "Folded": true
+  },
+  {
+    "Title": "The Empire Strikes Back",
+    "Version": "Style B",
+    "Film-Year": 1980,
+    "Size": [ 
+      "One-Sheet", 
+      "Lobby-Card", 
+      "Daybill" ],
+    "Folded": true
+  },
+  {
+    "Title": "Return of the Jedi",
+    "Version": "Advance",
+    "Film-Year": 1983,
+    "Size": [ 
+      "One-Sheet", 
+      "Lobby-Card", 
+      "Daybill" ],
+    "Folded": true
+  },
+  {
+    "Title": "Raiders of the Lost Ark",
+    "Version": "Regular",
+    "Film-Year": 1981,
+    "Size": [ 
+      "One-Sheet", 
+      "Lobby-Card", 
+      "Daybill" ],
+    "Folded": true
+  },
+  {
+    "Title": "Forbidden Planet",
+    "Version": "Regular",
+    "Film-Year": 1956,
+    "Size": [ 
+	"One-Sheet", 
+	"Lobby-Card", 
+	"Daybill" ],
+    "Folded": true,
+  }
+];
